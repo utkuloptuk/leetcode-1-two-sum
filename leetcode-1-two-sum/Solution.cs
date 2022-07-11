e6ba04aacee382a64e73f08e4a9790616f838bf8
UtkuLoptuk <utkuloptuk@gmail.com>
2022-07-11 16:49:55 +0300

    Add project files.

---
+++ leetcode-1-two-sum/Solution.cs
@@ -0,0 +1,46 @@
+﻿
+namespace leetcode_1_two_sum
+{
+    public static class Solution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var diff = target - nums[i];
+                if (nums.Contains(diff))
+                {
+                    var numsList = nums.ToList();
+                    var list = nums.Where(x => x == diff).ToList();
+                    if (numsList.IndexOf(diff).Equals(i))
+                        continue;
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                    result.Add(i);
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                   
+                    foreach(var l in list)
+                    {
+                        if(numsList.IndexOf(l)==i)
+                        {
+                            result.Add(numsList.LastIndexOf(diff));
+                            break;
+                        }
+                        else
+                        {
+                            result.Add(numsList.IndexOf(diff));
+                            break;
+                        }
+                    }
+                    if(result!=null)
+                    {
+                        break;
+                    }
+                }
+            }
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+            return result.ToArray();
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+        }
+    }
+}
---
+++ leetcode-1-two-sum/SolutionTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace leetcode_1_two_sum
+{
+    [TestClass]
+    public class SolutionTest
+    {
+        [DataRow(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0, 1 })]
+        [DataRow(new int[] { 3, 2, 4 }, 6, new int[] { 1, 2 })]
+        [DataRow(new int[] { -1, -2, -3, -4, -5 }, -8, new int[] { 2, 4 })]
+        [DataRow(new int[] { -3, 4, 3, 90 }, 0, new int[] { 0, 2 })]
+        [TestMethod]
+        public void NumberList_Compare_Target_After_Return_Succeed(int[] numbs, int target, int[] excepted_result)
+        {
+            var result = Solution.TwoSum(numbs, target);
+            Assert.IsTrue(result.SequenceEqual(excepted_result));
+        }
+    }
+}
